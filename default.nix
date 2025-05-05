fe6f0e28addbc16f9c76c642495a6a275e9e90ac
Ryan Omasta <git@ryand.ca>
2025-05-04 19:24:38 -0600

    feat: add nix flake

---
+++ default.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  maven,
+  runCommand,
+}:
+let 
+  rawJar = maven.buildMavenPackage rec {
+    pname = "essence-reloaded";
+    version = "1.9.0";
+
+    src = ./.;
+    mvnHash = "sha256-4j2yU+X1x+hYI/MTG3d8NRfqoFdIDM3djwvZnhRuG+4=";
+
+    installPhase = ''
+      runHook preInstall
+      cp target/essence-${version}.jar "$out"
+      runHook postInstall
+    '';
+
+    meta = {
+      description = "Stress-free Minecraft server utilities";
+      changelog = "https://github.com/ryand56/EssenceReloaded/releases/tag/${version}";
+      homepage = "https://github.com/ryand56/EssenceReloaded";
+      license = lib.licenses.asl20;
+    };
+  };
+in
+runCommand "${rawJar.name}.jar" { } ''
+  cp ${rawJar} $out
+''
+
---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "Essence: Stress-free server utilities.";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      # javaVersion = 22; # Change this value to update the whole stack
+
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.default ]; };
+      });
+    in
+    {
+      overlays.default =
+        final: prev: rec {
+          # jdk = prev."jdk${toString javaVersion}";
+          maven = prev.maven.override { jdk_headless = prev.jdk; };
+          gradle = prev.gradle.override { java = prev.jdk; };
+        };
+
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            gcc
+            gradle
+            jdk
+            maven
+            ncurses
+            patchelf
+            zlib
+          ];
+        };
+      });
+
+      packages = forEachSupportedSystem ({ pkgs }:
+        let
+          essence = pkgs.callPackage ./. { };
+        in
+        {
+          inherit essence;
+          default = essence;
+        });
+    };
+}
+
